dfa234541d0abdc0c94f3852596c4615de65046a
Cristhian <criskp007@gmail.com>
2022-03-28 10:42:57 -0400

    create form on home_screens

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -3,6 +3,22 @@ import 'package:flutter/material.dart';
 class HomeScreens extends StatelessWidget {
   const HomeScreens({Key? key}) : super(key: key);
 
+  Widget fieldPlate() {
+    return TextFormField(decoration: InputDecoration(labelText: 'Chapa'));
+  }
+
+  Widget fieldEmail() {
+    return TextFormField(decoration: InputDecoration(labelText: 'Marca'));
+  }
+
+  Widget fieldModel() {
+    return TextFormField(decoration: InputDecoration(labelText: 'Modelo'));
+  }
+
+  Widget fieldClock() {
+    return TextFormField(decoration: InputDecoration(labelText: 'Hora'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,62 +29,12 @@ class HomeScreens extends StatelessWidget {
           IconButton(onPressed: () {}, icon: Icon(Icons.car_rental_sharp))
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            AppBar(
-              leading: Icon(Icons.verified_user),
-              elevation: 0,
-              title: Text('Detalles Vehículo'),
-              backgroundColor: Theme.of(context).accentColor,
-              centerTitle: true,
-              actions: <Widget>[],
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 16, right: 16, top: 16),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Chapa',
-                  hintText: 'Chapa del Vehículo',
-                  icon: Icon(Icons.lock_outline),
-                  isDense: true,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 16, right: 16, bottom: 24),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Modelo',
-                  hintText: 'Modelo del Vehículo',
-                  icon: Icon(Icons.car_rental),
-                  isDense: true,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 16, right: 16, bottom: 24),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Marca',
-                  hintText: 'Marca del Vehículo',
-                  icon: Icon(Icons.dinner_dining),
-                  isDense: true,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 16, right: 16, bottom: 24),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Horario',
-                  hintText: '',
-                  icon: Icon(Icons.punch_clock),
-                  isDense: true,
-                ),
-              ),
-            )
-          ],
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Form(
+          child: Column(
+            children: [fieldPlate(), fieldEmail(), fieldModel(), fieldClock()],
+          ),
         ),
       ),
     );
