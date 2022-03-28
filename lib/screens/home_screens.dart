ee599a854effc4dab37ec9ca51c8d164105789ea
Cristhian <criskp007@gmail.com>
2022-03-28 11:50:40 -0400

    fix style home screen

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -6,25 +6,33 @@ class HomeScreens extends StatelessWidget {
   Widget fieldPlate() {
     return TextFormField(
         decoration: InputDecoration(
-            labelText: 'Chapa', icon: Icon(Icons.paste_rounded)));
+            labelText: 'Chapa',
+            icon: Icon(Icons.paste_rounded),
+            border: OutlineInputBorder()));
   }
 
   Widget fieldEmail() {
     return TextFormField(
         decoration: InputDecoration(
-            labelText: 'Marca', icon: Icon(Icons.paste_rounded)));
+            labelText: 'Marca',
+            icon: Icon(Icons.paste_rounded),
+            border: OutlineInputBorder()));
   }
 
   Widget fieldModel() {
     return TextFormField(
         decoration: InputDecoration(
-            labelText: 'Modelo', icon: Icon(Icons.receipt_long_rounded)));
+            labelText: 'Modelo',
+            icon: Icon(Icons.receipt_long_rounded),
+            border: OutlineInputBorder()));
   }
 
   Widget fieldClock() {
     return TextFormField(
         decoration: InputDecoration(
-            labelText: 'Hora', icon: Icon(Icons.calendar_month_rounded)));
+            labelText: 'Hora',
+            icon: Icon(Icons.calendar_month_rounded),
+            border: OutlineInputBorder()));
   }
 
   Widget buttonSave() {
@@ -59,16 +67,21 @@ class HomeScreens extends StatelessWidget {
           IconButton(onPressed: () {}, icon: Icon(Icons.car_rental_sharp))
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Form(
+      body: Form(
+        child: Padding(
+          padding: EdgeInsets.all(20),
           child: Column(
             children: [
               fieldPlate(),
+              SizedBox(height: 20),
               fieldEmail(),
+              SizedBox(height: 20),
               fieldModel(),
+              SizedBox(height: 20),
               fieldClock(),
+              SizedBox(height: 20),
               buttonSave(),
+              SizedBox(height: 20),
               buttonCancel(),
             ],
           ),
