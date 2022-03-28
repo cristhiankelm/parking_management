8e07974548fe2c191aa517821940175c5b08ff0e
Cristhian <criskp007@gmail.com>
2022-03-28 11:18:54 -0400

    create buttons on home screens

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -27,6 +27,28 @@ class HomeScreens extends StatelessWidget {
             labelText: 'Hora', icon: Icon(Icons.calendar_month_rounded)));
   }
 
+  Widget buttonSave() {
+    return SizedBox(
+        width: double.infinity,
+        height: 50,
+        child: ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(primary: Colors.green),
+            onPressed: () {},
+            icon: Icon(Icons.save),
+            label: Text('Guardar')));
+  }
+
+  Widget buttonCancel() {
+    return SizedBox(
+        width: double.infinity,
+        height: 50,
+        child: ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(primary: Colors.red),
+            onPressed: () {},
+            icon: Icon(Icons.cancel_presentation_rounded),
+            label: Text('Cancelar')));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +63,14 @@ class HomeScreens extends StatelessWidget {
         padding: EdgeInsets.all(10),
         child: Form(
           child: Column(
-            children: [fieldPlate(), fieldEmail(), fieldModel(), fieldClock()],
+            children: [
+              fieldPlate(),
+              fieldEmail(),
+              fieldModel(),
+              fieldClock(),
+              buttonSave(),
+              buttonCancel(),
+            ],
           ),
         ),
       ),
